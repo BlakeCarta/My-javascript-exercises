2d7bd394474da1ca0b08d71898a98049b17b86c0
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2024-08-13 15:33:49 -0500

    Exercise 6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 == 0){
+        //Leap
+        return true;
+    }
+    if (year % 100 == 0){
+        //not
+        return false;
+    }
+    if (year % 4 == 0){
+        //Leap
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
