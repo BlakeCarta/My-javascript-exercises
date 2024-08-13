32af58c128dc5d7acf45936d12123bbfd5fed791
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2024-08-13 15:10:45 -0500

    Redo this later

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //got stuck so I had to look it up
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
