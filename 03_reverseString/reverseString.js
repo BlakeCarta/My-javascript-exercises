69e0cf41151262e7e3f782ec168a715b70f7ba00
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2024-08-13 12:26:21 -0500

    Exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string.length == 0) {
+        return '';
+    }
+    let reversedString = [string.length];
+    for (let i=0; i < string.length; i++) {
+        reversedString[i] = string[string.length -1 - i];
+    }
+    return reversedString.join('');
 };
 
 // Do not edit below this line
