750f37734dd666e9f04a2fcd973cd12af45eda80
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2024-08-13 12:20:00 -0500

    Exercise 1 & 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let fullString = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    for (let index = 0; index < num; index++) {
+        fullString += string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
