0d0b90fa9e1dff549f7aff8a8eebd36afa701d0c
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2024-08-13 15:49:11 -0500

    Exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * (5/9);
+  let conTemp = Math.round(temp * 10)/10;
+  //console.log(`ToCels ${temp}`);
+  return conTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * (9/5) + 32);
+  let conTemp = Math.round(temp * 10)/10;
+  //console.log(`ToFahr ${temp}`);
+  return conTemp;
 };
 
 // Do not edit below this line
